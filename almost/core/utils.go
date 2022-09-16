ab84bbb18ce674f3405b00365d0d79a7f4ebeeef
Mirko <send@mirko.pm>
2022-09-16 11:37:52 +0200

    create the almost path on init

--- almost/core/utils.go
+++ almost/core/utils.go
@@ -5,6 +5,20 @@ import (
 	"os"
 )
 
+var almostDir = "/etc/almost"
+
+func init() {
+	if !RootCheck(false) {
+		return
+	}
+	if _, err := os.Stat(almostDir); os.IsNotExist(err) {
+		if err := os.Mkdir(almostDir, 0755); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
+}
+
 func RootCheck(display bool) bool {
 	if os.Geteuid() != 0 {
 		if display {
