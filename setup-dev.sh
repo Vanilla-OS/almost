50724b9e64d14025d2c3bf3030d532d550cb6773
Mirko Brombin <send@mirko.pm>
2022-09-23 14:38:33 +0200

    fix install script

--- setup-dev.sh
+++ setup-dev.sh
@@ -42,7 +42,7 @@ fi
 ln -s $(pwd)/almost /usr/bin/almost
 
 # Systemd unit
-ssudo cp systemd/almost.service /usr/lib/systemd/system/almost.service
+sudo cp systemd/almost.service /usr/lib/systemd/system/almost.service
 sudo systemctl daemon-reload
 sudo systemctl enable almost.service
 sudo systemctl start almost.service
